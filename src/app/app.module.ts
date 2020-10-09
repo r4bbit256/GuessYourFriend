a52c059b03a46a920b4ff51526312349ab26c01f
Alex Leo <leontev.mmaker@gmail.com>
2020-10-09 21:17:42 +0300

    Added basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { ResultsComponent } from './game/results/results.component';
+import { AddCardComponent } from './add-card/add-card.component';
+import { CardListComponent } from './card-list/card-list.component';
+import { ScoreComponent } from './score/score.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    ResultsComponent,
+    AddCardComponent,
+    CardListComponent,
+    ScoreComponent
   ],
   imports: [
     BrowserModule,
