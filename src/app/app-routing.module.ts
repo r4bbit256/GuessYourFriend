9def2400380aa9d957c362f7e0a2d79425ed0ac9
Alex Leo <leontev.mmaker@gmail.com>
2020-10-09 21:29:04 +0300

    * Added basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { CardListComponent } from './card-list/card-list.component';
+import { ScoreComponent } from './score/score.component';
+import { ResultsComponent } from './game/results/results.component';
+
 const routes: Routes = [
-  { path: '' }
+  { path: 'home', component: AppComponent },
+  { path: 'game', component: GameComponent,
+    children: [ { path: 'results', component: ResultsComponent }, ] },
+  { path: 'card-list', component: CardListComponent },
+  { path: 'score', component: ScoreComponent },
+  { path: '**', component: AppComponent }
 ];
 
 @NgModule({
