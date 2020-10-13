960916f5a05214352af1dd1d1b98cd74f6a7ffb3
Alex Leo <leontev.mmaker@gmail.com>
2020-10-13 16:55:10 +0300

    * Added LoggerService
    * Added unit tests for service

--- src/app/services/logger/logger.service.spec.ts
+++ src/app/services/logger/logger.service.spec.ts
@@ -8,9 +8,6 @@ describe('LoggerService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(LoggerService);
-    spyOn(window.console, 'log');
-    spyOn(window.console, 'warn');
-    spyOn(window.console, 'error');
   });
 
   it('service should be created', () => {
@@ -19,18 +16,21 @@ describe('LoggerService', () => {
 
   it('logInfo should log info data', () => {
     const date = new Date();
+    spyOn(window.console, 'log');
     service.logInfo('SomeErrorDescription');
     expect(window.console.log).toHaveBeenCalledWith(`@timestamp: ${date.toISOString()}, LogLevel: Info`, 'SomeErrorDescription');
   });
 
   it('logWarning should log warning data', () => {
     const date = new Date();
+    spyOn(window.console, 'warn');
     service.logWarning('SomeErrorDescription');
     expect(window.console.warn).toHaveBeenCalledWith(`@timestamp: ${date.toISOString()}, LogLevel: Warning`, 'SomeErrorDescription');
   });
 
   it('logError should log error data', () => {
     const date = new Date();
+    spyOn(window.console, 'error');
     service.logError('SomeErrorDescription');
     expect(window.console.error).toHaveBeenCalledWith(`@timestamp: ${date.toISOString()}, LogLevel: Error`, 'SomeErrorDescription');
   });
