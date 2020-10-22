a877bbb467a2069c7788cdf06d9ac809e3213102
Alex Leo <leontev.mmaker@gmail.com>
2020-10-22 18:56:47 +0300

    * Attempt to modify service

--- src/app/services/storage/storage.service.ts
+++ src/app/services/storage/storage.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class StorageService {
-
+// try to implement generic type for StorageService
   constructor() { }
 
   save(key: string, value: object): void {
@@ -13,6 +13,18 @@ export class StorageService {
 
   get(id: string): string {
     return localStorage.getItem(id);
+  }
+
+  getItem<TData, TResult>(key: string): Promise<TResult> {
+    return new Promise<TResult>((resolve, reject) => {
+      const result = JSON.parse(localStorage.getItem(key));
+      return result as TResult;
+      resolve();
+    });
+  }
+
+  update(id: string, value: object) {
+    // TODO
   }
 
   getAll(): string[] {
