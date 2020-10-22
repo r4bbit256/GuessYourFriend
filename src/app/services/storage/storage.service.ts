d86dbc93881039688aded8f0a33f114f3cb103ea
Alex Leo <leontev.mmaker@gmail.com>
2020-10-22 22:16:10 +0300

    * Removed unused code

--- src/app/services/storage/storage.service.ts
+++ src/app/services/storage/storage.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class StorageService {
-// try to implement generic type for StorageService
+
   constructor() { }
 
   save(key: string, value: object): void {
@@ -15,24 +15,12 @@ export class StorageService {
     return localStorage.getItem(id);
   }
 
-  getItem<TData, TResult>(key: string): Promise<TResult> {
-    return new Promise<TResult>((resolve, reject) => {
-      const result = JSON.parse(localStorage.getItem(key));
-      return result as TResult;
-      resolve();
-    });
-  }
-
-  update(id: string, value: object) {
-    // TODO
-  }
-
   getAll(): string[] {
     const values = [];
     const keys = Object.keys(localStorage);
     let i = keys.length;
 
-    while ( i-- ) {
+    while (i--) {
       values.push( localStorage.getItem(keys[i]) );
     }
 
