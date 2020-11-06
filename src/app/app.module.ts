c1eb667cb5e38bc0147118c142f47ae2097b55e4
Alex Leo <leontev.mmaker@gmail.com>
2020-11-06 20:19:31 +0200

    * Added new component about to provide info for guest users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,15 +13,19 @@ import { HomeComponent } from './home/home.component';
 import { CardsModule } from './cards/cards.module';
 import { GameModule } from './game/game.module';
 import { AuthModule } from './auth/auth.module';
+import { AboutComponent } from './about/about.component';
 
 const AngularMaterialModules = [
-  MatToolbarModule
+  MatToolbarModule,
+  MatButtonModule,
+  MatMenuModule
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
