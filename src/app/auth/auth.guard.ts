54dde585fc845abe0173a5e52c26d5d41e49a566
Alex Leo <leontev.mmaker@gmail.com>
2020-11-14 18:29:09 +0200

    * Moved routes to general file for future reuse

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -9,6 +9,7 @@ import {
 } from '@angular/router';
 
 import { AuthService } from '../services/auth/auth.service';
+import { ApiRoutes } from '../shared/utilities/api-routes';
 
 @Injectable({
   providedIn: 'root'
@@ -42,6 +43,6 @@ export class AuthGuard implements CanActivate, CanActivateChild {
 
     this.authService.redirectUrl = url;
 
-    return this.router.parseUrl('/auth/login');
+    return this.router.parseUrl(`/${ApiRoutes.Auth}/${ApiRoutes.Login}`);
   }
 }
--- src/app/shared/utilities/api-routes.ts
+++ src/app/shared/utilities/api-routes.ts
@@ -1,5 +1,10 @@
 export class ApiRoutes {
+  // Home
   public static Default = '/';
   public static Home = 'home';
   public static About = 'about';
+
+  // Authentication
+  public static Auth = 'auth';
+  public static Login = 'login';
 }
