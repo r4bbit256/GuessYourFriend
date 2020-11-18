b03409328dd871e457e316990febabac64828eff
Alex Leo <leontev.mmaker@gmail.com>
2020-11-18 18:13:12 +0200

    * Added new directive to highlight necessary items in app

---
+++ src/app/shared/directives/highlight.directive.ts
@@ -0,0 +1,29 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]',
+})
+export class HighlightDirective {
+  @Input() backgroundColor: string;
+  @Input() itemColor: string;
+
+  constructor(private el: ElementRef) {}
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.setBackgroundColor(this.backgroundColor || '#3f51b5');
+    this.setItemColor(this.itemColor || 'white');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setBackgroundColor(null);
+    this.setItemColor(null);
+  }
+
+  private setBackgroundColor(color: string): void {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+
+  private setItemColor(color: string): void {
+    this.el.nativeElement.style.color = color;
+  }
+}
