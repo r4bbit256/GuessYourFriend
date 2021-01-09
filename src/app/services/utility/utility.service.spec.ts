0597e858585ec13c7f4418ddd09c0a552fd65578
Alex Leo <leontev.mmaker@gmail.com>
2021-01-09 20:11:36 +0200

    * Added utility.service unit tests

--- src/app/services/utility/utility.service.spec.ts
+++ src/app/services/utility/utility.service.spec.ts
@@ -3,14 +3,41 @@ import { TestBed } from '@angular/core/testing';
 import { UtilityService } from './utility.service';
 
 describe('UtilityService', () => {
-  let service: UtilityService;
+  let utilityService: UtilityService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(UtilityService);
+    utilityService = TestBed.inject(UtilityService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(utilityService).toBeTruthy();
+  });
+
+  fit('#getUTCDateInMilliseconds return UTC Date in Miliseconds', () => {
+    // arrange
+    const date = new Date();
+    const year = date.getUTCFullYear();
+    const month = date.getUTCMonth();
+    const day = date.getUTCDay();
+    const hour = date.getUTCHours();
+    const minutes = date.getUTCMinutes() + 15;
+
+    // act
+    const dateInMiliseconds = utilityService.getUTCDateInMilliseconds(0, 0, 0, 0, 15);
+
+    // assert
+    expect(dateInMiliseconds).toEqual(Date.UTC(year, month, day, hour, minutes));
+  });
+
+  it('#getMinutesInMilliseconds return minutes in Miliseconds', () => {
+    // arrante
+    const requiredResult = 900000;
+
+    // act
+    const minutesInMiliseconds = utilityService.getMinutesInMilliseconds(15);
+
+    // assert
+    expect(minutesInMiliseconds).toEqual(requiredResult);
   });
 });
