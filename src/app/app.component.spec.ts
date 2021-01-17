49ee46f5788f905a8499db0810823a3b06aab05f
AlexLeoDEV <leontev.mmaker@gmail.com>
2021-01-17 15:56:27 +0200

    * Added app-component unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,46 @@
+import { Router } from '@angular/router';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MatMenuModule } from '@angular/material/menu';
+
+import { BehaviorSubject } from 'rxjs';
+
+import { MockAuthService } from './services/auth/mock.auth.service';
+import { AccountService } from './services/account/account.service';
+import { AuthService } from './services/auth/auth.service';
+import { TestRandomDataGeneratorModule } from './services/random-data-generator/test-random-data-generator.module';
+
+import { ApiRoutes } from './shared/utilities/api-routes';
+import { UserInterfaceResources } from './shared/utilities/user-interface.resources';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
+  let appComponent: AppComponent;
+  let authService: AuthService;
+  let accountService: AccountService;
+  let router: Router;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        TestRandomDataGeneratorModule,
+        MatMenuModule
+      ],
+      providers: [
+        AppComponent,
+        { provide: AuthService, useClass: MockAuthService },
+        { provide: AccountService, useClass: MockAccountService}
       ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+
+    router = TestBed.inject(Router);
+    appComponent = TestBed.inject(AppComponent);
+    authService = TestBed.inject(AuthService);
+    accountService = TestBed.inject(AccountService);
   });
 
   it('should create the app', () => {
@@ -20,10 +49,49 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
+  it('should render specific labels for non authorized guests', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('guess-your-friend app is running!');
+    expect(compiled.querySelector('.logo span').textContent).toEqual(UserInterfaceResources.AppNameLabel);
+    expect(compiled.querySelector('.username').textContent).toEqual(`${UserInterfaceResources.WelcomeMessageLabel} Guest`);
+    expect(compiled.querySelector('a.home').textContent).toEqual(UserInterfaceResources.HomeMenuLabel);
+    expect(compiled.querySelector('a.about').textContent).toEqual(UserInterfaceResources.AboutMenuLabel);
+    expect(compiled.querySelector('.copyright').textContent).toEqual(` © ${new Date().getFullYear()} ${UserInterfaceResources.AppNameLabel} - All Rights Reserved `);
+    expect(compiled.querySelector('.login').textContent).toEqual(UserInterfaceResources.LoginLabel.toUpperCase());
+    expect(compiled.querySelector('.register').textContent).toEqual(UserInterfaceResources.RegisterLabel.toUpperCase());
+  });
+
+  it('#ngOnInit sets initial user parameters', () => {
+    appComponent.ngOnInit();
+    expect(appComponent.isAuthenticated).toBeFalsy();
+    expect(appComponent.username).toEqual('Guest');
+  });
+
+  it('#logout logs out user and redirect to default route', () => {
+    spyOn(accountService, 'logout');
+    spyOn(router, 'navigate');
+    appComponent.logout();
+    expect(accountService.logout).toHaveBeenCalledTimes(1);
+    expect(router.navigate).toHaveBeenCalledWith([ApiRoutes.Default]);
+  });
+
+  it('#ngOnDestroy unsubscribes from parameter of AuthService', () => {
+    spyOn(authService.isLoggedIn, 'unsubscribe');
+    appComponent.ngOnDestroy();
+    expect(authService.isLoggedIn.unsubscribe).toHaveBeenCalled();
   });
 });
+
+class MockAccountService {
+  userName = new BehaviorSubject('Guest');
+
+  logout(): void {
+    this.clearCredentials();
+  }
+
+  private clearCredentials() {
+    localStorage.removeItem('currentUser');
+    localStorage.removeItem('expirationDate');
+  }
+}
--- src/app/services/auth/mock.auth.service.ts
+++ src/app/services/auth/mock.auth.service.ts
@@ -1,6 +1,9 @@
+import { BehaviorSubject } from 'rxjs';
 import { AuthorizationToken } from 'src/app/models/authorization-token';
 
 export class MockAuthService {
+  isLoggedIn = new BehaviorSubject(false);
+
   setCredentials(authToken: AuthorizationToken): void {
     localStorage.setValue('currentUser', authToken.user);
     localStorage.setValue('expirationDate', authToken.expirationDate);
