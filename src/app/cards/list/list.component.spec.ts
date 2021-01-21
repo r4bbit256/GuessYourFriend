6e96001d3a98bc07b147c942f90cf713d6968a86
Alex Leo <leontev.mmaker@gmail.com>
2021-01-21 17:50:13 +0200

    * Added list component tests

--- src/app/cards/list/list.component.spec.ts
+++ src/app/cards/list/list.component.spec.ts
@@ -1,19 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RandomDataGeneratorService } from './../../services/random-data-generator/random-data-generator.service';
 import { CardService } from './../../services/card/cards.service';
 import { MockCardsService } from 'src/app/services/card/mock.cards.service';
 import { TestRandomDataGeneratorModule } from 'src/app/services/random-data-generator/test-random-data-generator.module';
 
 import { ListComponent } from './list.component';
 import { Card } from 'src/app/models/card';
-import { MockRandomDataGeneratorService } from 'src/app/services/random-data-generator/mock-random-data-generator.service';
 
 fdescribe('CardListComponent', () => {
   let listComponent: ListComponent;
-  let randomDataService: MockRandomDataGeneratorService;
   let cardService: MockCardsService;
   let fixture: ComponentFixture<ListComponent>;
+  const cards: Card[] = [{
+    id: 'bbf2dcb6-f513-4440-a0e8-b59518b29165',
+    firstName: 'Melvin',
+    lastName: 'Perry',
+    gender: 'male',
+    photo: '35.jpg',
+    job: 'QA'
+  }];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -24,7 +29,6 @@ fdescribe('CardListComponent', () => {
       declarations: [ ListComponent ]
     }).compileComponents();
 
-    randomDataService = TestBed.inject(RandomDataGeneratorService);
     cardService = TestBed.inject(CardService);
   });
 
@@ -45,21 +49,18 @@ fdescribe('CardListComponent', () => {
     });
   });
 
-  fit('#generateCards generate specific number of cards', () => {
-    spyOn(randomDataService, 'getRandomJobList');
+  it('#generateCards generate specific number of cards', () => {
     spyOn(listComponent.cards$, 'next');
     spyOn(cardService, 'getAll');
     spyOn(cardService, 'addCards');
-    const cards: Card[] = [{
-      id: '6c306c4a-fd29-4e12-9f29-eb6c4b99eec5',
-      firstName: 'Melvin',
-      lastName: 'Perry',
-      gender: 'male',
-      photo: '35.jpg',
-      job: 'QA'
-    }];
     listComponent.generateCards('1');
     expect(cardService.addCards).toHaveBeenCalledWith(cards);
     expect(listComponent.cards$.next).toHaveBeenCalledWith(cardService.getAll());
+  });
+
+  it('#cardsUpdate updates card list', () => {
+    spyOn(listComponent.cards$, 'next');
+    listComponent.cardsUpdate(cards);
+    expect(listComponent.cards$.next).toHaveBeenCalledWith(cards);
   });
 });
