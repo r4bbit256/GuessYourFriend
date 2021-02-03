cc12907119d3a5c05f55f4894ed838bfafe1bd7a
AlexLeoDEV <leontev.mmaker@gmail.com>
2021-02-03 22:11:05 +0200

    * Removed unused code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
-import { AdItem } from '../shared/components/ad-banner/ad-item';
-import { AdService } from '../shared/services/ad.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  ads: AdItem[];
 
-  constructor(private adService: AdService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.ads = this.adService.getAds();
   }
 }
