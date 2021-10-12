ce8f852ca534d8a46823734b8b6db44fdd546722
AlexLeoDEV <leontev.mmaker@gmail.com>
2021-10-12 14:42:27 +0300

    * Added jenkins configuration
    * Modified prettier settings
    * Removed comments

--- src/app/services/api/base-api-requests.service.ts
+++ src/app/services/api/base-api-requests.service.ts
@@ -7,39 +7,39 @@ import { catchError, retry } from 'rxjs/operators';
 import { LoggerService } from '../logger/logger.service';
 
 const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-  }),
+    headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+    }),
 };
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class BaseApiRequestsService {
-  constructor(private http: HttpClient, private logger: LoggerService) {}
+    constructor(private http: HttpClient, private logger: LoggerService) {}
 
-  get<T>(url: string): Observable<T> {
-    return this.http.get<T>(url).pipe(retry(3), catchError(this.handleError));
-  }
-
-  post<T>(url: string, data: any): Observable<T> {
-    return this.http.post<T>(url, data, httpOptions).pipe(catchError(this.handleError));
-  }
-
-  delete(url: string, id: number): Observable<{}> {
-    return this.http.delete(`${url}/${id}`, httpOptions).pipe(catchError(this.handleError));
-  }
-
-  private handleError(error: HttpErrorResponse): Observable<any> {
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      this.logger.logError('An error occurred:', error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
-      this.logger.logError(`Backend returned code ${error.status}, body was: ${error.error}`);
+    get<T>(url: string): Observable<T> {
+        return this.http.get<T>(url).pipe(retry(3), catchError(this.handleError));
     }
 
-    return throwError('Something bad happened; please try again later.');
-  }
+    post<T>(url: string, data: any): Observable<T> {
+        return this.http.post<T>(url, data, httpOptions).pipe(catchError(this.handleError));
+    }
+
+    delete(url: string, id: number): Observable<{}> {
+        return this.http.delete(`${url}/${id}`, httpOptions).pipe(catchError(this.handleError));
+    }
+
+    private handleError(error: HttpErrorResponse): Observable<any> {
+        if (error.error instanceof ErrorEvent) {
+            // A client-side or network error occurred. Handle it accordingly.
+            this.logger.logError('An error occurred:', error.error.message);
+        } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong.
+            this.logger.logError(`Backend returned code ${error.status}, body was: ${error.error}`);
+        }
+
+        return throwError('Something bad happened; please try again later.');
+    }
 }
